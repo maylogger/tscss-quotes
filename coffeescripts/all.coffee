5b89a020f4e9f26fdd89d46a9e063d3e7c88a00a
gooooooooooooogle <kangxilord@gmail.com>
2015-01-09 02:18:54 +0800

    第一版

---
+++ coffeescripts/all.coffee
@@ -0,0 +1,37 @@
+$('.bigtext').bigtext()
+
+Tabletop.init
+  key: 'https://docs.google.com/spreadsheets/d/1C5Sdax2-et0u4-xys6XoFP-vH_gJ6SvtoIFtIQEM4w8/pubhtml',
+  key: 'https://docs.google.com/spreadsheets/d/1v7O_9whMC8qzUrExTVUpmt97N6VsXFMYR-EKrXsXQoQ/pubhtml',
+  callback: (data) =>
+    for column, i in data.response.column_names
+      if column isnt '時間戳記'
+        for el in data.response.elements
+          $('.sentence'+i).append('<div class="item"><div class="item-wrap"><div class="bigtext-item"><span>'+el[column]+'</span></div></div></div>')
+    $('.bigtext-item').bigtext({
+      minfontsize: 40
+    })
+    $('body').addClass('prepared')
+
+# $('.intro').click ->
+$('body').click ->
+  total1 = $('.sentence1 .item').length
+  total2 = $('.sentence2 .item').length
+  window.rand1 = Math.floor(Math.random()*(total1))
+  window.rand2 = Math.floor(Math.random()*(total2))
+  $('.sentence1 .item, .sentence2 .item').removeClass('in')
+  flip(10)
+  $('body').removeClass('prepared').addClass('start')
+
+flip = (i) ->
+  setTimeout (->
+    $('.sentence1').find('.item').removeClass('in')
+    $('.sentence1').find('.item:eq(' + (window.rand1 - i) + ')').addClass('in')
+    if --i then flip i else flip2(10)
+  ), 80
+flip2 = (i) ->
+  setTimeout (->
+    $('.sentence2').find('.item').removeClass('in')
+    $('.sentence2').find('.item:eq(' + (window.rand1 - i) + ')').addClass('in')
+    flip2 i if --i
+  ), 80
---
+++ javascripts/all.js
@@ -0,0 +1,65 @@
+(function() {
+  var flip, flip2,
+    _this = this;
+
+  $('.bigtext').bigtext();
+
+  Tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/1C5Sdax2-et0u4-xys6XoFP-vH_gJ6SvtoIFtIQEM4w8/pubhtml',
+    key: 'https://docs.google.com/spreadsheets/d/1v7O_9whMC8qzUrExTVUpmt97N6VsXFMYR-EKrXsXQoQ/pubhtml',
+    callback: function(data) {
+      var column, el, i, _i, _j, _len, _len1, _ref, _ref1;
+
+      _ref = data.response.column_names;
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        column = _ref[i];
+        if (column !== '時間戳記') {
+          _ref1 = data.response.elements;
+          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+            el = _ref1[_j];
+            $('.sentence' + i).append('<div class="item"><div class="item-wrap"><div class="bigtext-item"><span>' + el[column] + '</span></div></div></div>');
+          }
+        }
+      }
+      $('.bigtext-item').bigtext({
+        minfontsize: 40
+      });
+      return $('body').addClass('prepared');
+    }
+  });
+
+  $('body').click(function() {
+    var total1, total2;
+
+    total1 = $('.sentence1 .item').length;
+    total2 = $('.sentence2 .item').length;
+    window.rand1 = Math.floor(Math.random() * total1);
+    window.rand2 = Math.floor(Math.random() * total2);
+    $('.sentence1 .item, .sentence2 .item').removeClass('in');
+    flip(10);
+    return $('body').removeClass('prepared').addClass('start');
+  });
+
+  flip = function(i) {
+    return setTimeout((function() {
+      $('.sentence1').find('.item').removeClass('in');
+      $('.sentence1').find('.item:eq(' + (window.rand1 - i) + ')').addClass('in');
+      if (--i) {
+        return flip(i);
+      } else {
+        return flip2(10);
+      }
+    }), 80);
+  };
+
+  flip2 = function(i) {
+    return setTimeout((function() {
+      $('.sentence2').find('.item').removeClass('in');
+      $('.sentence2').find('.item:eq(' + (window.rand1 - i) + ')').addClass('in');
+      if (--i) {
+        return flip2(i);
+      }
+    }), 80);
+  };
+
+}).call(this);
